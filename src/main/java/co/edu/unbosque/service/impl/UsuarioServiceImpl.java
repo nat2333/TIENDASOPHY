f9bfe1ac842bfb1ab985674fcb2f821e59bb0d5c
nat2333 <lolmosp@unbosque.edu.co>
2025-05-15 21:55:58 -0500

    Nuevo primer commit limpio

---
+++ src/main/java/co/edu/unbosque/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,28 @@
+package co.edu.unbosque.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
+
+import co.edu.unbosque.utils.GenericServiceImpl;
+import co.edu.unbosque.entity.Usuario;
+import co.edu.unbosque.service.api.UsuarioServiceAPI;
+import co.edu.unbosque.repository.UsuarioRepository;
+
+@Service
+public class UsuarioServiceImpl extends GenericServiceImpl<Usuario, Integer> implements UsuarioServiceAPI {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@Override
+	public CrudRepository<Usuario, Integer> getDao() {
+		return usuarioRepository;
+	}
+
+	@Override
+	public Usuario get(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
